de262cc55082808aa4e611cdad2ebe9da3f7f7bb
modao233 <13556366913@163.com>
2020-11-14 14:19:21 +0800

    Site updated: 2020-11-14 14:19:08

--- js/copyright.js
+++ js/copyright.js
@@ -8,7 +8,7 @@
         // Selection 对象，表示用户选择的文本范围或光标的当前位置。
         //     声明一个变量接收 -- 用户输入的剪切或者复制的文本转化为字符串
         let text = window.getSelection().toString();
-    
+		    
         if (text) {
             // 如果文本存在则先取消文本默认事件
             event.preventDefault();
@@ -20,7 +20,7 @@
             + '\n著作权归作者所有，商业转载请联系作者获得授权，非商业转载请注明出处。'
             + '\n作者: modao'
             + '\n邮箱: rustacean@aliyun.com'
-            + '\n原文地址: <%= page.permalink %>'
+            //+ '\n原文地址: <%= page.permalink %>'
 			
             clipboardData.setData('text/plain', text + copyright);
     
@@ -34,3 +34,7 @@
     contents[0].addEventListener('copy',function(e){
         setClipboardText(e);
     });
+
+<% if (!page.nocopyright) { %>
+	<script src="<%- config.root %>js/copyright.js"></script>
+<% } %>
